4c876bf730c77f1ec233a7a2fb34129f84c28c2c
manoleaioan <ioanmanolea@gmail.com>
2020-04-08 12:37:59 +0300

    adding header component and link to home, shop page and contact

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -13,7 +13,6 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
-
     </div>
   </div>
 );
