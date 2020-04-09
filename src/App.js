20c5be39c78bfeb3aab607e708c9caa0c501f979
manoleaioan <ioanmanolea@gmail.com>
2020-04-09 16:31:04 +0300

    implemented firebase utils included ability to store authenticated users into firestore database

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
@@ -21,25 +21,37 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+       
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      }else{
+        this.setState({ currentUser: userAuth });
+      }
 
-      console.log(user);
     });
   }
-  
+
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
 
-  render() { 
+  render() {
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
         <Switch>
-         <Route exact path='/' component={HomePage} />
-         <Route exact path='/shop' component={ShopPage} />
-         <Route exact path='/signin' component={SignInAndSignUp} />
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUp} />
         </Switch>
       </div>
     );
