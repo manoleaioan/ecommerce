23d930f8ecc99610ddf3174a17986da878594a2a
manoleaioan <ioanmanolea@gmail.com>
2020-04-11 15:54:21 +0300

    updated shop data with new data and updated cart utils to group items in  cart reducer

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
+import { Redirect } from 'react-router-dom';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
