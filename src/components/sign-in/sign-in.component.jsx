6c69e1b1a2778bf85e2a67094391d7a1bb0692b4
manoleaioan <ioanmanolea@gmail.com>
2020-04-16 15:27:42 +0300

    removing redux-logger from production build

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
+import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -13,7 +14,7 @@ class SignIn extends React.Component {
     this.state = {
       email: '',
       password: ''
-    }
+    };
   }
 
   handleSubmit = async event => {
@@ -21,49 +22,48 @@ class SignIn extends React.Component {
 
     const { email, password } = this.state;
 
-    try{
+    try {
       await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email:'', password:'' });
-    }catch(err){
-      console.log(err);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
-
-    this.setState({ email: '', password: '' })
-  }
+  };
 
   handleChange = event => {
-    const { value, name } = event.target
+    const { value, name } = event.target;
 
-    this.setState({ [name]: value })
-  }
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
+
         <form onSubmit={this.handleSubmit}>
           <FormInput
-            name="email"
-            type="email"
-            value={this.state.email}
+            name='email'
+            type='email'
             handleChange={this.handleChange}
-            label="email"
+            value={this.state.email}
+            label='email'
             required
           />
           <FormInput
-            name="password"
-            type="password"
+            name='password'
+            type='password'
             value={this.state.password}
             handleChange={this.handleChange}
-            label="password"
+            label='password'
             required
           />
           <div className='buttons'>
-            <CustomButton type="submit">Sign In</CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              Sign In with Google
-          </CustomButton>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
           </div>
         </form>
       </div>
@@ -71,4 +71,4 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn
+export default SignIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
-import { Redirect } from 'react-router-dom';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUp = () => (
   <div className='sign-in-and-sign-up'>
